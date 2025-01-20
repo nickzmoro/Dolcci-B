8862e4c11873fa7282912e4eb895aeaca82e1c76
nickzmoro <nicolasmororuiz@gmail.com>
2025-01-20 20:55:50 -0300

    add see more & js

---
+++ assets/js/details.js
@@ -0,0 +1,43 @@
+// veja mais (bombom) | fechar
+btnSeeMoreBombom.addEventListener("click", () => {
+    detailsBombom.style.visibility = "visible";
+    mask.style.visibility = "visible";
+})
+
+btnCloseBombom.addEventListener("click", () => {
+    detailsBombom.style.visibility = "hidden";
+    mask.style.visibility = "hidden";
+})
+
+// veja mais (enroladinho) | fechar
+btnSeeMoreEnrolado.addEventListener("click", () => {
+    detailsEnrolado.style.visibility = "visible";
+    mask.style.visibility = "visible";
+})
+
+btnCloseEnrolado.addEventListener("click", () => {
+    detailsEnrolado.style.visibility = "hidden";
+    mask.style.visibility = "hidden";
+})
+
+// veja mais (bolacha) | fechar
+btnSeeMoreBolacha.addEventListener("click", () => {
+    detailsBolacha.style.visibility = "visible";
+    mask.style.visibility = "visible";
+})
+
+btnCloseBolacha.addEventListener("click", () => {
+    detailsBolacha.style.visibility = "hidden";
+    mask.style.visibility = "hidden";
+})
+
+// veja mais (bala) | fechar
+btnSeeMoreBala.addEventListener("click", () => {
+    detailsBala.style.visibility = "visible";
+    mask.style.visibility = "visible";
+})
+
+btnCloseBala.addEventListener("click", () => {
+    detailsBala.style.visibility = "hidden";
+    mask.style.visibility = "hidden";
+})
---
+++ assets/js/dom.js
@@ -0,0 +1,21 @@
+// botões
+const btnSeeMoreBombom = document.querySelector(".btn-see-more-bombom");
+const btnSeeMoreEnrolado = document.querySelector(".btn-see-more-enrolado");
+const btnSeeMoreBolacha = document.querySelector(".btn-see-more-bolacha");
+const btnSeeMoreBala = document.querySelector(".btn-see-more-bala");
+
+const btnCloseBombom = document.querySelector("#btn-close-bombom");
+const btnCloseEnrolado = document.querySelector("#btn-close-enrolado");
+const btnCloseBolacha = document.querySelector("#btn-close-bolacha");
+const btnCloseBala = document.querySelector("#btn-close-bala");
+
+// detalhes --sabores--
+const detailsBombom = document.querySelector(".details-bombom");
+const detailsEnrolado = document.querySelector(".details-enrolados");
+const detailsBolacha = document.querySelector(".details-bolacha");
+const detailsBala = document.querySelector(".details-balas");
+
+const detailsAll = document.getElementsByClassName("details-flavors");
+
+// máscara
+const mask = document.querySelector(".mask");
